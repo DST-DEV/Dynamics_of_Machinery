a9674d2f52d3d722ec520c781a952ffe2e2ed905
s250244 <s250244@student.dtu.dk>
2026-02-20 15:38:19 +0100

    buggy code and alternative 3d

--- model_dummy.py
+++ model_dummy.py
@@ -33,6 +33,15 @@ k4 = 2 * (12 * E * I1) / l4**3
 k5 = (3 * E * I2) / l5**3
 k6 = (3 * E * I2) / l6**3
 
+#Forces Matrix
+P1 = np.array([0, -m1*g, 0])
+P2 = np.array([0, -m2*g, 0])
+P3 = np.array([0, -m3*g, 0])
+P4 = np.array([0, -m4*g, 0])
+P5 = np.array([0, -m5*g, 0])
+P6 = np.array([0, -m6*g, 0])
+
+
 # Integrator settings
 deltaT = 1e-5
 n_int = int(8e6)
@@ -42,18 +51,19 @@ theta = np.zeros(n_int)
 theta[0] = np.pi / 2
 thetad = np.zeros(n_int)
 
-x1 = np.zeros(n_int)
-x1d = np.zeros(n_int)
-x2 = np.zeros(n_int)
-x2d = np.zeros(n_int)
-x3 = np.zeros(n_int)
-x3d = np.zeros(n_int)
-x4 = np.zeros(n_int)
-x4d = np.zeros(n_int)
-x5 = np.zeros(n_int)
-x5d = np.zeros(n_int)
-x6 = np.zeros(n_int)
-x6d = np.zeros(n_int)
+I_1 = np.zeros((3, n_int))
+I_1d = np.zeros((3, n_int))
+I_2 = np.zeros((3, n_int))
+I_2d = np.zeros((3, n_int))
+I_3 = np.zeros((3, n_int))
+I_3d = np.zeros((3, n_int))
+I_4 = np.zeros((3, n_int))
+I_4d = np.zeros((3, n_int))
+I_5 = np.zeros((3, n_int))
+
+I_5d = np.zeros((3, n_int))
+I_6 = np.zeros((3, n_int))
+I_6d = np.zeros((3, n_int))
 
 # Position vectors
 Iroa = np.array([x1[0], l1, 0])
@@ -62,6 +72,7 @@ Irbc = np.array([x3[0], l3, 0])
 Ircd = np.array([x4[0], l4, 0])
 B5rde = np.array([x5[0], l5, 0])
 B5rdf = np.array([x6[0], -l6, 0])
+
 
 # Transformation matrix
 Ttheta = np.array([
@@ -77,88 +88,61 @@ Irod = Iroa + Irab + Irbc + Ircd
 Iroe = Iroa + Irab + Irbc + Ircd + np.dot(Ttheta.T, B5rde)
 Irof = Iroa + Irab + Irbc + Ircd + np.dot(Ttheta.T, B5rdf)
 
-rx_a = np.zeros(n_int)
-ry_a = np.zeros(n_int)
-rz_a = np.zeros(n_int)
-rx_a[0] = Iroa[0]
-ry_a[0] = Iroa[1]
-rz_a[0] = Iroa[2]
+# creating the position vectors describing masses trajectories
+I_r_a = np.zeros((3,n_int)) #
+I_r_a[0] = Iroa
 
-rx_b = np.zeros(n_int)
-ry_b = np.zeros(n_int)
-rz_b = np.zeros(n_int)
-rx_b[0] = Irob[0]
-ry_b[0] = Irob[1]
-rz_b[0] = Irob[2]
-
-rx_c = np.zeros(n_int)
-ry_c = np.zeros(n_int)
-rz_c = np.zeros(n_int)
-rx_c[0] = Iroc[0]
-ry_c[0] = Iroc[1]
-rz_c[0] = Iroc[2]
-
-rx_d = np.zeros(n_int)
-ry_d = np.zeros(n_int)
-rz_d = np.zeros(n_int)
-rx_d[0] = Irod[0]
-ry_d[0] = Irod[1]
-rz_d[0] = Irod[2]
-
-rx_e = np.zeros(n_int)
-ry_e = np.zeros(n_int)
-rz_e = np.zeros(n_int)
-rx_e[0] = Iroe[0]
-ry_e[0] = Iroe[1]
-rz_e[0] = Iroe[2]
-
-rx_f = np.zeros(n_int)
-ry_f = np.zeros(n_int)
-rz_f = np.zeros(n_int)
-rx_f[0] = Irof[0]
-ry_f[0] = Irof[1]
-rz_f[0] = Irof[2]
-
-# Reaction forces
-React_y1 = np.zeros(n_int)
-React_y2 = np.zeros(n_int)
-React_y3 = np.zeros(n_int)
-React_y4 = np.zeros(n_int)
-React_y5 = np.zeros(n_int)
-React_y6 = np.zeros(n_int)
-
-React_y1[0] = k5 * m5 * x5[0]
-
-React_y2[0] = k5 * m5 * x5[0] * np.sin(theta[0])
-
-React_y3[0] = 4 * g * m4**2 + 4 * g * m3 * m4
-
-React_y4[0] = 4 * g * m4**2 + 8 * m4 * thetad[0] * x5[0] * np.cos(theta[0])
+I_r_b = np.zeros((3,n_int))
+I_r_b[0] = Irob
 
 
-x1dd = np.zeros(n_int)
-x2dd = np.zeros(n_int)
-x3dd = np.zeros(n_int)
-x4dd = np.zeros(n_int)
-x5dd = np.zeros(n_int)
-x6dd = np.zeros(n_int)
+I_r_c = np.zeros((3,n_int))
+I_r_c[0] = Iroc
+
+I_r_d = np.zeros((3,n_int))
+I_r_d[0] = Irod
+
+I_r_e = np.zeros((3,n_int)))
+I_r_e[0] = Iroe
+
+
+I_r_f = np.zeros((3,n_int))
+I_r_f[0] = Irof
+
+# Reaction forces TODO
+I_T1 = np.zeros((3,n_int))
+I_T2 = np.zeros((3,n_int))
+I_T3 = np.zeros((3,n_int))
+I_T4 = np.zeros((3,n_int))
+I_T5 = np.zeros((3,n_int))
+I_T6 = np.zeros((3,n_int))
+
+
+
+#TODO
+I_1dd = np.zeros((3,n_int))
+I_2dd = np.zeros((3,n_int))
+I_3dd = np.zeros((3,n_int))
+I_4dd = np.zeros((3,n_int))
+I_5dd = np.zeros((3,n_int))
+I_6dd = np.zeros((3,n_int))
 t_int = np.zeros(n_int)
 
 for i in range(1, n_int):
     t_int[i-1] = (i-2)*deltaT
 
     # accelerations
-    x1dd[i-1] = (-k1*x1[i-1] + k2*x2[i-1]) / m1
+    I_1dd[0,i-1] = (-k1*x1[i-1] + k2*x2[i-1]) / m1
 
-    x2dd[i-1] = (k1*m2*x1[i-1] - k2*m1*x2[i-1] - k2*m2*x2[i-1] + k3*m1*x3[i-1]) / (m1*m2)
+    I_2dd[0,i-1] = (k1*m2*x1[i-1] - k2*m1*x2[i-1] - k2*m2*x2[i-1] + k3*m1*x3[i-1]) / (m1*m2)
 
-    x3dd[i-1] = (k2*m3*x2[i-1] - k3*m2*x3[i-1] - k3*m3*x3[i-1] + k4*m2*x4[i-1]) / (m2*m3)
+    I_3dd[0,i-1] = (k2*m3*x2[i-1] - k3*m2*x3[i-1] - k3*m3*x3[i-1] + k4*m2*x4[i-1]) / (m2*m3)
 
-    x4dd[i-1] = 2*k4*m3*x4[i-1] - 2*k3*m4*x3[i-1] + 2*k4*m4*x4[i-1]
+    I_4dd[0,i-1] = 2*k4*m3*x4[i-1] - 2*k3*m4*x3[i-1] + 2*k4*m4*x4[i-1]
 
-    x5dd[i-1] = m5 * thetad[i-1]**2 * x5[i-1]
+    I_5dd[0,i-1] = m5 * thetad[i-1]**2 * x5[i-1]
 
-    x6dd[i-1] = -m5 * thetad[i-1]**2 * x6[i-1]
+    I_6dd[0,i-1] = -m5 * thetad[i-1]**2 * x6[i-1]
 
     # velocities
     x1d[i] = x1d[i-1] + x1dd[i-1]*deltaT
@@ -178,17 +162,17 @@ for i in range(1, n_int):
     x6[i] = x6[i-1] + x6d[i-1]*deltaT
 
     # reaction forces
-    React_y1[i] = k5*m5*x5[i]*np.sin(theta[i])
+    React_1[i,1] = k5*m5*x5[i]*np.sin(theta[i])
 
-    React_y2[i] = k5*m5*x5[i]*np.sin(theta[i])
+    React_2[i,1] = k5*m5*x5[i]*np.sin(theta[i])
 
-    React_y3[i] = 4*g*m4**2
+    React_3[i,1] = 4*g*m4**2
 
-    React_y4[i] = 4*g*m4**2
+    React_4[i,1] = 4*g*m4**2
 
-    React_y5[i] = 2*m4*thetad[i]*x5[i]
+    React_5[i,1] = 2*m4*thetad[i]*x5[i]
 
-    React_y6[i] = 2*m4*thetad[i]*x6[i]
+    React_6[i,1] = 2*m4*thetad[i]*x6[i]
 
     # transformation matrix
     Ttheta = np.array([
